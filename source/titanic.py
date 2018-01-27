9264063d8162447704d4e6c9f707fda5c9177a6b
Katie Li <katiesli16@gmail.com>
2018-01-26 17:19:54 -0800

    halfway done with c

--- source/titanic.py
+++ source/titanic.py
@@ -168,29 +168,45 @@ def error(clf, X, y, ntrials=100, test_size=0.2) :
     ### ========== TODO : START ========== ###
     # part b: compute cross-validation error over ntrials
     # hint: use train_test_split (be careful of the parameters)
-  SEED = 1234
+    SEED = 1234
     np.random.seed(SEED)
 
     train_error = 0
     test_error = 0
 
-    X_train = []; X_test = []
-    y_train = []; y_test = []
-    for i in range (0, trials):
-        if (np.random.random_sample() < .8):
-            train_set.append(X[i,:])
-            y_train.append(y[i])
-        else:
-            test_set.append(X[i,:])
-            y_test.append(y[i])
+    test_error_total = 0
+    train_error_total = 0
 
-        clf = DecisionTreeClassifier()
-        clf.fit([X_train,y_train])
+    n, d = X.shape
+
+
+
+    for t in range (0, ntrials):
+        # get the split of data
+        X_train = np.empty((0,d)); X_test = np.empty((0,d))
+        y_train = []; y_test = []
+
+        for i in range(0, n):
+            if (np.random.random_sample() < (1.0 - test_size)):
+                X_train = np.vstack((X_train, X[i,:]))
+                y_train.append(y[i])
+            else:
+                X_test = np.vstack((X_test,X[i,:]))
+                y_test.append(y[i])
+
+        # print "X_train shape is", X_train.shape
+        # print "shape of first element of X_train is:", X[0,:].shape
+        # print "shape of second elt:", X[1,:].shape
+        # print "shape of stack is:", np.vstack((X[0,:],X[1,:])).shape
+        # print "y train length is:", len(y)
+        clf.fit(X_train,y_train)
         y_pred_test = clf.predict(X_test)
         y_pred_train = clf.predict(X_train)
-        test_error += 1 - metrics.accuracy_score(y_pred_test, y_test, normalize=True)
-        train_error += 1 - metrics.accuracy_score(y_pred_train, y_train, normalize=True)
+        test_error_total += 1 - metrics.accuracy_score(y_pred_test, y_test, normalize=True)
+        train_error_total += 1 - metrics.accuracy_score(y_pred_train, y_train, normalize=True)
 
+    train_error = train_error_total/ntrials
+    test_error = test_error_total/ntrials
     ### ========== TODO : END ========== ###
 
     return train_error, test_error
@@ -262,6 +278,18 @@ def main():
     # part b: use cross-validation to compute average training and test error of classifiers
     print 'Investigating various classifiers...'
 
+    print 'MajorityVoteClassifier Error'
+    train_error, test_error = error(MajorityVoteClassifier() , X, y, ntrials=100, test_size=0.2)
+    print '\t-- training error: %.3f' % train_error
+    print '\t-- test error: %.3f' % test_error
+
+    print 'DecisionTreeClassifier Error'
+    train_error, test_error = error(DecisionTreeClassifier(criterion="entropy"), X, y, ntrials=100, test_size=0.2)
+    print '\t-- training error: %.3f' % train_error
+    print '\t-- test error: %.3f' % test_error
+
+
+
     ### ========== TODO : END ========== ###
 
 
@@ -269,6 +297,23 @@ def main():
     ### ========== TODO : START ========== ###
     # part c: investigate decision tree classifier with various depths
     print 'Investigating depths...'
+    train_error_arr = []
+    test_error_arr =[]
+    depths = range(1,21)
+    for depth in depths:
+        clf = DecisionTreeClassifier(criterion="entropy", max_depth=depth)
+        train_error, test_error = error(clf,X,y)
+        train_error_arr.append(train_error)
+        test_error_arr.append(test_error)
+
+    plt.plot(depths, train_error_arr, label = "train error")
+    plt.plot(depths, test_error_arr, label = "test error")
+
+    plt.xlabel("Max Depth of Decision Tree")
+    plt.ylabel("Average Error")
+    plt.legend()
+    plt.show()
+
 
     ### ========== TODO : END ========== ###
 
@@ -277,6 +322,25 @@ def main():
     ### ========== TODO : START ========== ###
     # part d: investigate decision tree classifier with various training set sizes
     print 'Investigating training set sizes...'
+    train_error_arr = []
+    test_error_arr =[]
+    split_sizes =  map (lambda u: u*.05,  range(1,20))
+    for split in split_sizes:
+        clf = DecisionTreeClassifier(criterion="entropy", max_depth=6)
+        train_error, test_error = error(clf,X,y,test_size=split)
+        train_error_arr.append(train_error)
+        test_error_arr.append(test_error)
+
+    training_splits = map (lambda u: 1 - u,  split_sizes)
+
+    plt.plot(training_splits, train_error_arr, label = "train error")
+    plt.plot(training_splits, test_error_arr, label = "test error")
+
+    plt.xlabel("Training Data Size")
+    plt.ylabel("Average Error")
+    plt.legend()
+    plt.show()
+
 
     ### ========== TODO : END ========== ###
 
