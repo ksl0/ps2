450285e310d53e6f65d1a071fd1c8a834a91b26c
Katie Li <katiesli16@gmail.com>
2018-03-27 15:46:06 -0700

    mostly done with 2b)

--- ps8/source/faces.py
+++ ps8/source/faces.py
@@ -26,23 +26,23 @@ from cluster import *
 def build_face_image_points(X, y) :
     """
     Translate images to (labeled) points.
-    
+
     Parameters
     --------------------
         X     -- numpy array of shape (n,d), features (each row is one image)
         y     -- numpy array of shape (n,), targets
-    
+
     Returns
     --------------------
         point -- list of Points, dataset (one point for each image)
     """
-    
+
     n,d = X.shape
-    
+
     images = collections.defaultdict(list) # key = class, val = list of images with this class
     for i in xrange(n) :
         images[y[i]].append(X[i,:])
-    
+
     points = []
     for face in images :
         count = 0
@@ -65,7 +65,7 @@ def plot_clusters(clusters, title, average) :
                     determines how to calculate average of points in cluster
                     allowable: ClusterSet.centroids, ClusterSet.medoids
     """
-    
+
     plt.figure()
     np.random.seed(20)
     label = 0
@@ -87,21 +87,21 @@ def plot_clusters(clusters, title, average) :
 def generate_points_2d(N, seed=1234) :
     """
     Generate toy dataset of 3 clusters each with N points.
-    
+
     Parameters
     --------------------
         N      -- int, number of points to generate per cluster
         seed   -- random seed
-    
+
     Returns
     --------------------
         points -- list of Points, dataset
     """
     np.random.seed(seed)
-    
+
     mu = [[0,0.5], [1,1], [2,0.5]]
     sigma = [[0.1,0.1], [0.25,0.25], [0.15,0.15]]
-    
+
     label = 0
     points = []
     for m,s in zip(mu, sigma) :
@@ -109,7 +109,7 @@ def generate_points_2d(N, seed=1234) :
         for i in xrange(N) :
             x = util.random_sample_2d(m, s)
             points.append(Point(str(label)+'_'+str(i), label, x))
-    
+
     return points
 
 
@@ -120,39 +120,35 @@ def generate_points_2d(N, seed=1234) :
 def random_init(points, k) :
     """
     Randomly select k unique elements from points to be initial cluster centers.
-    
+
     Parameters
     --------------------
         points         -- list of Points, dataset
         k              -- int, number of clusters
-    
+
     Returns
     --------------------
         initial_points -- list of k Points, initial cluster centers
     """
     ### ========== TODO : START ========== ###
     # part 2c: implement (hint: use np.random.choice)
-
-
-    return np.random.choice(points, k)
-
-
+    return np.random.choice(points, k, replace=False)
     ### ========== TODO : END ========== ###
 
 
 def cheat_init(points) :
     """
     Initialize clusters by cheating!
-    
+
     Details
     - Let k be number of unique labels in dataset.
     - Group points into k clusters based on label (i.e. class) information.
     - Return medoid of each cluster as initial centers.
-    
+
     Parameters
     --------------------
         points         -- list of Points, dataset
-    
+
     Returns
     --------------------
         initial_points -- list of k Points, initial cluster centers
@@ -167,30 +163,30 @@ def cheat_init(points) :
 def kMeans(points, k, init='random', plot=False) :
     """
     Cluster points into k clusters using variations of k-means algorithm.
-    
+
     Parameters
     --------------------
         points  -- list of Points, dataset
         k       -- int, number of clusters
         init    -- string, method of initialization
-                   allowable: 
+                   allowable:
                        'cheat'  -- use cheat_init to initialize clusters
                        'random' -- use random_init to initialize clusters
         plot    -- bool, True to plot clusters with corresponding averages
                          for each iteration of algorithm
-    
+
     Returns
     --------------------
         k_clusters -- ClusterSet, k clusters
     """
-    
+
     ### ========== TODO : START ========== ###
     # part 2c: implement
     # part 2d: allow for different initialization
     #
     # Hints:
     #   (1) On each iteration, keep track of the new cluster assignments
-    #       in a separate data structure. Then use these assignments to 
+    #       in a separate data structure. Then use these assignments to
     #       create new Cluster objects and a new ClusterSet object. Then
     #       update the centroids.
     #   (2) Repeat until the clustering no longer changes.
@@ -199,6 +195,7 @@ def kMeans(points, k, init='random', plot=False) :
     k_clusters.members = [None for i in range(k)]
 
     centroids = random_init(points, k)
+    iteration = 0;
 
     while True:
         cluster_assignments = [[] for i in range(k)]
@@ -210,15 +207,23 @@ def kMeans(points, k, init='random', plot=False) :
         for j in range(len(cluster_assignments)):
             k_clusters.members[j] = Cluster(cluster_assignments[j])
 
+        if (plot):
+            title = "Iteration " + str(iteration)
+            plot_clusters(k_clusters, title, ClusterSet.centroids)
+
         new_centroids = map(lambda u : u.centroid(), k_clusters.members)
 
         old_centroid_attrs = map(lambda u : u.attrs, centroids)
         new_centroid_attrs = map(lambda u : u.attrs, new_centroids)
-        
+
+        iteration += 1
+
         if np.array_equal(old_centroid_attrs,new_centroid_attrs):
             break
         else:
             centroids=new_centroids
+
+
 
     return k_clusters
 
@@ -264,78 +269,78 @@ def main() :
     #     new_X = util.reconstruct_from_PCA(Z, ul, mu)
 
     #     util.plot_gallery([new_X[j] for j in xrange(12)])
-    
-    
+
+
 
     ### ========== TODO : END ========== ###
-    
-    
-    
+
+
+
     #========================================
     # part 2
-    
+
     # part b: test Cluster implementation
     # centroid: [ 1.04022358  0.62914619]
-    
+
     np.random.seed(1234)
     sim_points = generate_points_2d(20)
     cluster = Cluster(sim_points)
     print 'centroid:', cluster.centroid().attrs
-    
+
     # parts c-d: test kMeans implementation using toy dataset
     np.random.seed(1234)
     sim_points = generate_points_2d(20)
     k = 3
-    
+
     # test cluster using random initialization
     kmeans_clusters = kMeans(sim_points, k, init='random', plot=True)
-    
+
     # test cluster using cheat initialization
-    kmeans_clusters = kMeans(sim_points, k, init='cheat', plot=True)
-    
-    
-    
+    # kmeans_clusters = kMeans(sim_points, k, init='cheat', plot=True)
+
+
+
     ### ========== TODO : START ========== ###
     # part 3
-    
+
     # part a: explore effect of lower-dimensional representations on clustering performance
     np.random.seed(1234)
-    
+
     # part b: determine ``most discriminative'' and ``least discriminative'' pairs of images
     np.random.seed(1234)
-    
+
     ### ========== TODO : END ========== ###
-    
-    
-    
+
+
+
     #========================================
     # part 4a
-    
+
     # test Cluster implementation
     # medoid:   [ 1.05674064  0.71183522]
-    
+
     np.random.seed(1234)
     sim_points = generate_points_2d(20)
     cluster = Cluster(sim_points)
     print 'medoid:', cluster.medoid().attrs
-    
+
     # test kMedoids implementation using toy dataset
     np.random.seed(1234)
     sim_points = generate_points_2d(20)
     k = 3
-    
+
     # test cluster using random initialization
     kmedoids_clusters = kMedoids(sim_points, k, init='random', plot=True)
-    
+
     ### ========== TODO : START ========== ###
     # part 4
-    
+
     # part b: compare k-means and k-medoids
     np.random.seed(1234)
-    
+
     # part c: explore effect of lower-dimensional representations on clustering performance
     np.random.seed(1234)
-    
+
     ### ========== TODO : END ========== ###
 
 
